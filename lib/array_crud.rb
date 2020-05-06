2d7ea36c8df38dd057862e9e3e3e7d024aff397a
malcolm staso <mstaso1@gmail.com>
2020-05-06 18:40:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ end
 
 def remove_element_from_start_of_array(array)
   colors = ["wow", "red", "blue", "green", "orange"]
-  
+  colors.shift
 end
 
 def retrieve_element_from_index(array, index_number)
