49af0a444ee70e8c5e07efd4e66a302cb7bd3fe6
malcolm staso <mstaso1@gmail.com>
2020-05-06 18:35:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,8 +16,8 @@ def add_element_to_start_of_array(array, element)
   colors.unshift ("wow")
 end
 
-def remove_element_from_end_of_array(colors)
-  arrays!= colors.pop
+def remove_element_from_end_of_array(array)
+  
 end
 
 
