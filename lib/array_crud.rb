727885f34cd110e569efbf7565931a2e6d48ac3e
malcolm staso <mstaso1@gmail.com>
2020-05-06 18:21:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  colors = [red, blue, green, orange]
+  colors = ["red", "blue", "green", "orange"]
   colors << "purple"
 end
 
