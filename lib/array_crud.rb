c4c1612c0be0201502e594540b12cfd57bb4129f
malcolm staso <mstaso1@gmail.com>
2020-05-06 18:37:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+   colors = ["wow", "red", "blue", "green", "orange", "arrays!"]
+   "arrays!" = colors.pop
 end
 
 
